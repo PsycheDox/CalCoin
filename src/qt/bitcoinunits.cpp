4a8f2e5543a82c75ea68a4a2ab3a2e260a42c79a
PsycheDox <psychedox@gmail.com>
2014-11-02 22:04:32 +0000

    updates

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BAR");
-    case mBTC: return QString("mBAR");
-    case uBTC: return QString::fromUtf8("μBAR");
+    case BTC: return QString("CAL");
+    case mBTC: return QString("mCAL");
+    case uBTC: return QString::fromUtf8("μCAL");
     default: return QString("???");
     }
 }
